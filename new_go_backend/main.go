5c09f17c38a216bdffab50351c636bae2c0487ec
Milanka00 <milankatharangana01@gmail.com>
2024-03-06 10:40:31 +0530

    sleep time set

--- new_go_backend/main.go
+++ new_go_backend/main.go
@@ -3,14 +3,25 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
+    "strconv"
+    "time"
 )
 
 func main() {
     // Define routes
-    http.HandleFunc("/nocache", NoCacheHandler)
-    http.HandleFunc("/publiccache", PublicCacheHandler)
-    http.HandleFunc("/privatecache", PrivateCacheHandler)
-    http.HandleFunc("/getresponse", getresponseWithoutHeaders)
+    http.HandleFunc("/nocache", func(w http.ResponseWriter, r *http.Request) {
+        NoCacheHandler(w, r)
+    })
+    http.HandleFunc("/publiccache", func(w http.ResponseWriter, r *http.Request) {
+        PublicCacheHandler(w, r)
+    })
+    http.HandleFunc("/privatecache", func(w http.ResponseWriter, r *http.Request) {
+        PrivateCacheHandler(w, r)
+    })
+    http.HandleFunc("/getresponse", func(w http.ResponseWriter, r *http.Request) {
+        getresponseWithoutHeaders(w, r)
+    })
 
     // Start server
     fmt.Println("Server is listening on port 8000...")
@@ -21,20 +32,33 @@ func main() {
 func NoCacheHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Cache-Control", "no-store")
     fmt.Fprintf(w, "Response with Cache-Control: no-store")
+    sleepBeforeRespond()
 }
 
 // Handler for "/publiccache" route with Cache-Control: public header
 func PublicCacheHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Cache-Control", "public, max-age=3600")
     fmt.Fprintf(w, "Response with Cache-Control: public, max-age=3600")
+    sleepBeforeRespond()
 }
 
 // Handler for "/privatecache" route with Cache-Control: private header
 func PrivateCacheHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Cache-Control", "private, max-age=3600")
     fmt.Fprintf(w, "Response with Cache-Control: private, max-age=3600")
+    sleepBeforeRespond()
 }
 
 func getresponseWithoutHeaders(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Response without cache control headers")
+    sleepBeforeRespond()
+}
+
+func sleepBeforeRespond() {
+    sleepTimeStr := os.Getenv("SLEEP_TIME")
+    sleepTime, err := strconv.Atoi(sleepTimeStr)
+    if err != nil {
+        sleepTime = 15
+    }
+    time.Sleep(time.Duration(sleepTime) * time.Second)
 }
